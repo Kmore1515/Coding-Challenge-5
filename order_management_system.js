c82804f2a33f168cfd0ab531d6ae171c5d063ea8
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-19 21:39:42 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,7 @@
+// Task 1: Create an Inventory Array of Product Objects
+const inventory = [
+    {name: "Phone", price: 800, quantity: 20},
+    {name: "Tablet", price: 450, quantity: 15},
+    {name: "Television", price: 300, quantity: 10},
+    {name: "Laptop", price: 600, quantity: 25}
+];
